6dbe37fa226184a68e857dd59dae40c054511743
JeffVenancius <43701418+JeffVenancius@users.noreply.github.com>
2025-01-28 11:19:13 -0300

    Update index.jsx

--- src/components/Category/index.jsx
+++ src/components/Category/index.jsx
@@ -91,7 +91,7 @@ function Version(props) {
 
 function Category(props) {
 	console.log(props.titulo)
-	console.log(props.cards)
+	console.log(props.cards.length)
 	if (!props.cards) return <></>
   return (
     <div
